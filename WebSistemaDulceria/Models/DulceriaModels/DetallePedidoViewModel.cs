4e8b7c9deb367fb8939c5c2ce319264c9505eb9a
abner <abner15jh@gmail.com>
2023-01-02 13:15:52 -0600

    Avance de interfaz y componentes de pedidos

---
+++ WebSistemaDulceria/Models/DulceriaModels/DetallePedidoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSistemaDulceria.Models.DulceriaModels
+{
+    public class DetallePedidoViewModel
+    {
+        public int IdDetallePedido { get; set; }
+
+        public int IdPedido { get; set; }
+
+        public int IdArticulo { get; set; }
+
+        public decimal Cantidad { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public decimal Subtotal { get; set; }
+
+        public decimal Descuento { get; set; }
+
+        public ArticuloViewModel Articulo { get; set; }
+
+    }
+}
---
+++ WebSistemaDulceria/Models/DulceriaModels/PedidoViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSistemaDulceria.Models.DulceriaModels
+{
+    public class PedidoViewModel
+    {
+
+        public int IdPedido { get; set; }
+
+        [Required(ErrorMessage = "Proveedor es requerido")]
+        public int IdProveedor { get; set; }
+
+        [Required(ErrorMessage = "Numero Factura es requerido")]
+        public decimal? NumeroFactura { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Observaciones { get; set; }
+
+        public decimal Subtotal { get; set; }
+
+        public decimal Descuento { get; set; }
+
+        public decimal Iva { get; set; }
+
+        public decimal Total { get; set; }
+
+        public int? NumeroReferencia { get; set; }
+
+        [Required]
+        public DateTime FechaCreacion { get; set; }
+
+        [Required]
+        public int IdUsuarioCreacion { get; set; }
+
+        public DateTime FechaModificacion { get; set; }
+
+        public int IdUsuarioModificacion { get; set; }
+
+        public List<DetallePedidoViewModel> DetallePedido { get; set; } = new List<DetallePedidoViewModel>();
+
+        public ProveedoresViewModel Proveedor { get; set; } = new ProveedoresViewModel();
+
+    }
+}
