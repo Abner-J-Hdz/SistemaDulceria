701ddf37a1fc557315a7f814fa0f18b0a5502815
AbnerMartinez01 <122928340+AbnerMartinez01@users.noreply.github.com>
2023-02-08 00:35:20 -0600

    cambio en el nombre del archivo

--- WebSistemaDulceria/wwwroot/js/report.js
+++ WebSistemaDulceria/wwwroot/js/report.js
@@ -23,3 +23,27 @@ function crearPDF() {
         doc.save('FacturaDeVenta.pdf');
     });
 }
+
+
+function crearPDFReporteVentas() {
+
+    var quotes = document.getElementById('factura');
+
+    html2canvas(quotes).then(function (canvas) {
+        var imgData = canvas.toDataURL('iamge/png')
+        var imgWidth = 210;
+        var pageHeight = 295;
+        var imgHeight = canvas.height * imgWidth / canvas.width;
+        var heightLeft = imgHeight;
+        /*var doc = new jsPDF({
+            orientation: "landscape",
+            unit: "in",
+            format: [4, 2]
+        });*/
+
+        var doc = new jsPDF('p', 'mm', 'a4')
+        var position = 0;
+        doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        doc.save('ResumenDeVentaDelDia.pdf');
+    });
+}
